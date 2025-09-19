75eea8a81eacfc52fb1d665680af9deabc6e9e01
nguyent-111 <nguyent@newlands.school.nz>
2025-09-19 13:23:59 +1200

    Initial Commit

---
+++ Assets/Sprites/TextMesh Pro/Scripts/Kill.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    public playerHealth pHealth;
+    public float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pHealth.health -= damage;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pHealth.health -= damage;
+        }
+    }
+}
